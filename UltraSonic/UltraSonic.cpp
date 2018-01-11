9ef0e9f0a8c1cba9ce65153b694dccdd0b89d9af
Patipon Taweechat <57050269@kmitl.ac.th>
2018-01-11 16:41:15 +0700

    Add UltraSonic Library

---
+++ UltraSonic/UltraSonic.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+int TRIGGER_PIN;
+int ECHO_PIN; 
+
+UltraSonic::UltraSonic(int pin1, int pin2)
+{
+  TRIGGER_PIN = pin1;
+  ECHO_PIN = pin2;
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+double UltraSonic::readDistance()
+{
+  long duration, distance;
+  digitalWrite(TRIGGER_PIN, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+  delay(1000);
+  return distance;
+}
